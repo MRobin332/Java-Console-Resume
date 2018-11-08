fcd70a124f37df11ce0fdca5a6845754a85fb52e
Mitch Robinson <mitchellallenrobinson@gmail.com>
2018-11-07 19:05:55 -0600

    modified code in JS file

---
+++ JavaResumeConsole.js
@@ -0,0 +1,31 @@
+let personalInformation = {
+    Name: 'Mitch Robinson',
+    Career: 'Web Developer / Inspirational Speaker',
+    Description: 'While not what one would refer to as "inspiring" in the most literal sense, I do hold my own when it comes to being passably underwhelming.' 
+}
+
+let myInterests = {
+    'Alabama Football',
+    'Guitar',
+}
+
+let pastPositions = {
+    Videorama: 'Manager',
+    Blockbuster: 'Accountant',
+    Construction: 'Shovel Holder',
+}
+
+let skills = {
+    1: 'BAM: Plays Guitar',
+    2: 'Wow. Not too bad at playing the guitar',
+    3: 'Well, ok, not the best, but still pretty good at playing the guitar!',
+    4: 'Further Results Pending',
+}
+
+let allCaps = personalInformation.Name.toUpperCase();
+
+console.log (allCaps);
+console.log (personalInformation.Career);
+console.log (personalInformation.Description);
+
+console.log (myInterests);
